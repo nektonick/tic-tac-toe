54f4bb00c5c0472e52bf28a27106633ad652f7ed
Nick Grebnev <nik-1905@mail.ru>
2022-04-05 23:45:36 +0300

    Main file added

---
+++ main.cpp
@@ -0,0 +1,34 @@
+﻿#include <cstdlib>
+#ifdef USE_UI
+#    include <QApplication>
+#    include "mainwindow.h"
+#else
+#    include <iostream>
+#endif
+
+
+#ifdef USE_UI
+int show_ui(int argc, char* argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
+#else
+int show_console(int argc, char* argv[])
+{
+    std::cout << "App is under development" << std::endl;
+    return EXIT_SUCCESS;
+}
+#endif
+
+
+int main(int argc, char* argv[])
+{
+#ifdef USE_UI
+    return show_ui(argc, argv);
+#else
+    return show_console(argc, argv);
+#endif
+}
