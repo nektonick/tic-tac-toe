9840dd3e539dae79e7092c178a8867efae160416
Nick Grebnev <nik-1905@mail.ru>
2022-04-08 00:41:54 +0300

    Players classes added

---
+++ src/core/player.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+enum class PlayerType
+{
+    AI,
+    human
+};
+
+
+class I_Player
+{
+public:
+    I_Player() = default;
+    virtual ~I_Player() = default;
+
+    virtual PlayerType getType() const noexcept = 0;
+};
+
+
+class AI_Player final : public I_Player
+{
+public:
+    PlayerType getType() const noexcept
+    {
+        return PlayerType::AI;
+    }
+};
+
+
+class Human_Player final : public I_Player
+{
+public:
+    PlayerType getType() const noexcept
+    {
+        return PlayerType::human;
+    }
+};
