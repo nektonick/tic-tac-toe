488edb3b2fd4eea52d2ad9ae38e81a5d754a3569
Nick Grebnev <nik-1905@mail.ru>
2022-04-08 00:39:50 +0300

    base types moved into new file

---
+++ src/core/base_types.cpp
@@ -0,0 +1,1 @@
+﻿#include "base_types.h"
---
+++ src/core/base_types.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include <cstdint>
+#include <stdexcept>
+
+
+enum class CellContent
+{
+    x, // cross
+    o // nought
+};
+
+
+class CellPosition
+{
+public:
+    // we start counting from zero
+    CellPosition(int64_t row, int64_t column)
+        : row_(row)
+        , column_(column)
+    {
+        if(row_ < 0) {
+            throw std::invalid_argument("Invalid row");
+        }
+        if(column_ < 0) {
+            throw std::invalid_argument("Invalid column");
+        }
+    }
+
+    uint64_t getRow() const noexcept
+    {
+        return row_;
+    }
+
+    uint64_t getColumn() const noexcept
+    {
+        return column_;
+    }
+
+private:
+    const uint64_t row_;
+    const uint64_t column_;
+};
