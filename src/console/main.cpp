ba828b9c2c1814c32f70b40fc2c48954d9e05267
Nick Grebnev <nik-1905@mail.ru>
2022-04-09 23:49:54 +0300

    main files added to executable targets

---
+++ src/console/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <cstdlib>
+#include <iostream>
+#include "game.h"
+
+
+// TODO: get players types and filed size from params
+int run_in_console(int argc, char* argv[])
+{
+    Game game(GameSettings(FieldSize(), PlayerType::human, PlayerType::AI));
+    std::cout << "App is under development" << std::endl;
+    return EXIT_SUCCESS;
+}
+
+
+int main(int argc, char* argv[])
+{
+    return run_in_console(argc, argv);
+}
---
+++ src/gui/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <QApplication>
+#include <cstdlib>
+#include "game.h"
+#include "mainwindow.h"
+
+
+// TODO: get players types and filed size from params
+int run_in_gui(int argc, char* argv[])
+{
+    Game game(GameSettings(FieldSize(), PlayerType::human, PlayerType::AI));
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
+
+
+int main(int argc, char* argv[])
+{
+    return run_in_gui(argc, argv);
+}
