fce99dcdd825f3b42b4fd2697d9bfe136095229d
Nick Grebnev <nik-1905@mail.ru>
2022-04-11 23:16:46 +0300

    player module updated

--- src/core/player.cpp
+++ src/core/player.cpp
@@ -1,26 +1,59 @@
 ﻿#include "player.h"
 
+
+I_Player::I_Player(MarkType player_mark)
+    : mark_(player_mark)
+{
+}
+
+AI_Player::AI_Player(MarkType player_mark)
+    : I_Player(player_mark)
+{
+}
+
 PlayerType AI_Player::getType() const noexcept
 {
     return PlayerType::AI;
 }
 
+CellPosition AI_Player::selectCellToMark()
+{
+    // TODO
+    return CellPosition(0, 0);
+}
+
+MarkType I_Player::getMark() const noexcept
+{
+    return mark_;
+}
+
+
+Human_Player::Human_Player(MarkType player_mark)
+    : I_Player(player_mark)
+{
+}
 
 PlayerType Human_Player::getType() const noexcept
 {
     return PlayerType::human;
 }
 
+CellPosition Human_Player::selectCellToMark()
+{
+    // TODO
+    return CellPosition(0, 0);
+}
 
-std::unique_ptr<I_Player> PlayersFabric::getPlayerOfType(PlayerType type)
+
+std::unique_ptr<I_Player> PlayersFabric::getPlayerOfType(PlayerType type, MarkType mark)
 {
     std::unique_ptr<I_Player> player;
     switch(type) {
     case PlayerType::AI:
-        player = std::make_unique<AI_Player>();
+        player = std::make_unique<AI_Player>(mark);
         break;
     case PlayerType::human:
-        player = std::make_unique<Human_Player>();
+        player = std::make_unique<Human_Player>(mark);
         break;
     }
     return player;
--- src/core/player.h
+++ src/core/player.h
@@ -1,5 +1,15 @@
 ﻿#pragma once
 #include <memory> // unique_ptr
+#include <vector>
+#include "base_types.h"
+
+
+// Forward declaration
+class Cell;
+using CellsRow = std::vector<Cell>;
+using CellsGrid = std::vector<CellsRow>;
+class I_InputOutput; // TODO: use it
+
 
 enum class PlayerType
 {
@@ -11,29 +21,42 @@ enum class PlayerType
 class I_Player
 {
 public:
-    I_Player() = default;
+    I_Player(MarkType player_mark);
     virtual ~I_Player() = default;
 
     virtual PlayerType getType() const noexcept = 0;
+    MarkType getMark() const noexcept;
+    virtual CellPosition selectCellToMark() = 0;
+
+private:
+    const MarkType mark_;
 };
 
 
 class AI_Player final : public I_Player
 {
 public:
-    PlayerType getType() const noexcept;
+    AI_Player(MarkType player_mark);
+
+    PlayerType getType() const noexcept override;
+    CellPosition selectCellToMark() override;
+
+private:
 };
 
 
 class Human_Player final : public I_Player
 {
 public:
-    PlayerType getType() const noexcept;
+    Human_Player(MarkType player_mark);
+
+    PlayerType getType() const noexcept override;
+    CellPosition selectCellToMark() override;
 };
 
 
 class PlayersFabric
 {
 public:
-    static std::unique_ptr<I_Player> getPlayerOfType(PlayerType);
+    static std::unique_ptr<I_Player> getPlayerOfType(PlayerType, MarkType);
 };
