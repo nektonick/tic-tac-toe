30f1f0a86bd43e85760f3135b3f5fe59b65c899f
Nick Grebnev <nik-1905@mail.ru>
2022-04-14 01:20:39 +0300

    ai core stub added

---
+++ src/core/ai_core.cpp
@@ -0,0 +1,12 @@
+﻿#include "ai_core.h"
+
+AI_Core::AI_Core(std::shared_ptr<Field> field)
+    : field_(field)
+{
+}
+
+CellPosition AI_Core::makeMove()
+{
+    // TODO
+    return CellPosition(0, 0);
+}
---
+++ src/core/ai_core.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <memory>
+#include "field.h"
+
+// TODO: difficulty level
+class AI_Core
+{
+public:
+    AI_Core(std::shared_ptr<Field>);
+    CellPosition makeMove();
+
+private:
+    std::shared_ptr<Field> field_;
+};
