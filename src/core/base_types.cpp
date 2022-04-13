64ec3810f7e8dc6edf630cb19919e043a1206d43
Nick Grebnev <nik-1905@mail.ru>
2022-04-14 02:00:23 +0300

    warning fix

--- src/core/base_types.cpp
+++ src/core/base_types.cpp
@@ -4,10 +4,10 @@ CellPosition::CellPosition(int64_t row, int64_t column)
     : row_(row)
     , column_(column)
 {
-    if(row_ < 0) {
+    if(row < 0) {
         throw std::invalid_argument("Invalid row");
     }
-    if(column_ < 0) {
+    if(column < 0) {
         throw std::invalid_argument("Invalid column");
     }
 }
